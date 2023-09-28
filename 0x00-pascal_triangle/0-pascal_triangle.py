9f36e273cb004e4223e75260a737e487401ba98c
Tayo Jubril <jubriltayo@gmail.com>
2023-09-28 23:22:23 +0100

    Pascal triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 """A module to display Pascal's triangle"""
+
+
 def pascal_triangle(n):
     """
     Displays Pascal's triangle of n size
 
     Parameters:
         n (int): size of triangle
-    
+
     Returns:
-        triangle: list of list of triangle of size n 
+        triangle: list of list of triangle of size n
     """
     triangle = []
     for i in range(n):
