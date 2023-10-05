572ae806ab8d8a09bdc796a8652b64ca1791f9f6
Tayo Jubril <jubriltayo@gmail.com>
2023-10-05 22:06:12 +0100

    lockboxes

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This module cotnains function canUnlockAll for boxes"""
+
+def canUnlockAll(boxes):
+    """
+    This function determines if all boxes can opened or not 
+    """
+    if type(boxes) != list or len(boxes) == 0:
+        return False
+
+    box_length = len(boxes)
+    free_keys = [0]
+    used_keys = set()
+
+    while free_keys:
+        box_index = free_keys.pop()
+        used_keys.add(box_index)
+        opened_box = boxes[box_index]
+
+        if type(opened_box) != list:
+            return False
+
+        for key in opened_box:
+            if (key < box_length) and (key not in free_keys) and \
+               (key not in used_keys):
+                free_keys.append(key)
+
+    return len(used_keys) == box_length
