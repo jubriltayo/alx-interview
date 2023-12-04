583d3352cf10012b4301f807dbe5733113edcf25
Tayo Jubril <jubriltayo@gmail.com>
2023-12-05 00:20:50 +0100

    prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+""" This module defines the function `isWinner` """
+
+
+def isWinner(x, nums):
+    """ Returns the winner """
+    p1 = 0
+    p2 = 0
+    for round in range(x):
+        n = nums[round]
+        prime_counter = 0
+        for j in range(1, n + 1):
+            if isPrime(j):
+                prime_counter += 1
+
+        if prime_counter % 2 == 0:
+            p1 += 1
+        else:
+            p2 += 1
+
+        if p1 == p2:
+            return None
+        elif p1 > p2:
+            return 'Maria'
+        else:
+            return 'Ben'
+
+
+def isPrime(n):
+    if n == 2:
+        return True
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+            else:
+                return True
