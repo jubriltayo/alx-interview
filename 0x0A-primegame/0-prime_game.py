577c0f7a64289e22ed33676b6c006d2575ccccae
Tayo Jubril <jubriltayo@gmail.com>
2023-12-05 22:50:48 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,53 +1,38 @@
 #!/usr/bin/python3
-'''Prime Game'''
+""" This module defines the function `isWinner` """
 
 
 def isWinner(x, nums):
-    '''
-    Prototype: def isWinner(x, nums)
-    x is the number of rounds and nums is an array of n
-    Return: name of the player that won the most rounds
-    If the winner cannot be determined, return None
-    You can assume n and x will not be larger than 10000
-    '''
-    def is_prime(n):
-        '''Function to check if a number is prime'''
-        if n <= 1:
-            return False
-        if n <= 3:
-            return True
-        if n % 2 == 0 or n % 3 == 0:
-            return False
-        i = 5
-        while i * i <= n:
-            if n % i == 0 or n % (i + 2) == 0:
-                return False
-            i += 6
-        return True
+    """ Returns the winner """
+    p1 = 0
+    p2 = 0
+    for round in range(x):
+        n = nums[round]
+        prime_counter = 0
+        for j in range(1, n + 1):
+            if isPrime(j):
+                prime_counter += 1
 
-    def count_primes(n):
-        '''Function to count prime numbers in a range'''
-        count = 0
-        for i in range(2, n + 1):
-            if is_prime(i):
-                count += 1
-        return count
-
-    maria_wins = 0
-    ben_wins = 0
-
-    # Iterate through each round
-    for n in nums:
-        prime_count = count_primes(n)
-        if prime_count % 2 == 0:
-            ben_wins += 1
+        if prime_counter % 2 == 0:
+            p1 += 1
         else:
-            maria_wins += 1
+            p2 += 1
 
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
-        return "Ben"
-    else:
-        return None
+        if p1 == p2:
+            return None
+        elif p1 > p2:
+            return 'Maria'
+        else:
+            return 'Ben'
+
+
+def isPrime(n):
+    """Checks if a number is Prime or not"""
+    if n == 2:
+        return True
+    if n > 1:
+        for i in range(2, n):
+            if (n % i) == 0:
+                return False
+            else:
+                return True
