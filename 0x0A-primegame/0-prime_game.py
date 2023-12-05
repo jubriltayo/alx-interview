b692193924cbd27bfaa9bc2350de796aeb9e6645
Tayo Jubril <jubriltayo@gmail.com>
2023-12-05 18:42:08 +0100

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -28,11 +28,9 @@ def isWinner(x, nums):
 
 def isPrime(n):
     """Checks if a number is Prime or not"""
-    if n == 2:
-        return True
-    if n > 1:
-        for i in range(2, n):
-            if (n % i) == 0:
-                return False
-            else:
-                return True
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if (n % i) == 0:
+            return False
+    return True
